4c5635e00bf381e4c55bfb6cc42c29410aa8098e
Aquaindy <pete.nyandeh@gmail.com>
2022-04-29 21:32:56 -0400

    Initial Commit

---
+++ TravelSwiftUI/TravelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSwiftUIApp.swift
+//  TravelSwiftUI
+//
+//  Created by Pete Nyandeh Chie on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
